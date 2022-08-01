abd975217e14699d2c9212677cb2bf3a5bc94dd1
rola75 <cloudgit.241c2@simplelogin.co>
2022-08-01 14:30:12 -0500

    changed images in the for text

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const screenPrevious = document.querySelector('#screenPrevious');
 const screenCurrent = document.querySelector('#screenCurrent');
-const numButtons = document.querySelectorAll('.numButton');
+const numButtons = document.querySelectorAll('.numButton').value;
 const operationButtons = document.querySelectorAll('.opButton');
 const decimialButton = document.querySelector('#deciButton');
 const equalsButton = document.querySelector('#equalsButton');
@@ -8,4 +8,12 @@ const negativeButton = document.querySelector('#negativeButton');
 const backSpaceButton = document.querySelector('#backSpaceButton');
 const clearCurrentScreen = document.querySelector('#clearcurrentScreen');
 const clearBothScreens =document.querySelector('#clearBothScreens');
-let number = "";
+
+// figure out how to get a loop to stop on the number clicked
+const numButtonLoop = (numButtons) => {
+    numButtons.map((button) => {
+        return numButtons.map;
+    }) 
+}
+
+console.log(numButtonLoop[2]);
