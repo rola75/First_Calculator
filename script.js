b87ce1d80b664fd1571590034475eda2fbf68d87
rola75 <cloudgit.241c2@simplelogin.co>
2022-08-01 17:08:32 -0500

    add functionality to number buttons and clearing buttons

--- script.js
+++ script.js
@@ -1,19 +1,60 @@
 const screenPrevious = document.querySelector('#screenPrevious');
 const screenCurrent = document.querySelector('#screenCurrent');
-const numButtons = document.querySelectorAll('.numButton').value;
+const numButtons = document.querySelectorAll('.numButton');
 const operationButtons = document.querySelectorAll('.opButton');
 const decimialButton = document.querySelector('#deciButton');
 const equalsButton = document.querySelector('#equalsButton');
 const negativeButton = document.querySelector('#negativeButton');
 const backSpaceButton = document.querySelector('#backSpaceButton');
-const clearCurrentScreen = document.querySelector('#clearcurrentScreen');
+const clearCurrentScreen = document.querySelector('#clearCurrentScreen');
 const clearBothScreens =document.querySelector('#clearBothScreens');
 
+
+let currentNumber;
+let previoudNumber;
+let operator;
+
+const handleBackSpaceButton = () => {
+    screenCurrent.slice(0, -1);
+  };
+  
+backSpaceButton.addEventListener("click", handleBackSpaceButton);
+
+const handleCurrentScreen = () => {
+    screenCurrent.innerHTML = "";
+  };
+  
+clearCurrentScreen.addEventListener("click", handleCurrentScreen);
+  
+const handleClearBothScreens = () => {
+    screenCurrent.innerHTML = "";
+    screenPrevious.innerHTML = "";
+  };
+  
+clearBothScreens.addEventListener("click", handleClearBothScreens);
+
 // figure out how to get a loop to stop on the number clicked
-const numButtonLoop = (numButtons) => {
-    numButtons.map((button) => {
-        return numButtons.map;
-    }) 
+//Handler--button need to input it value
+//forLoop--iterate through the numButtons
+// const button9 = document.querySelector('#button9');
+// const handleButton9 = () => {
+//     screenCurrent.innerHTML = button9.innerHTML;
+// }
+// button9.addEventListener('click', handleButton9);
+const handleNumButtons = (event) => {
+screenCurrent.innerHTML += event.target.innerHTML;
+};
+
+for (let i = 0; i < numButtons.length; i++) {
+    numButtons[i].addEventListener('click', handleNumButtons);
 }
 
-console.log(numButtonLoop[2]);
+
+const handleOpButtons = (event) => {
+    current;
+    };
+    
+for (let i = 0; i < operationButtons.length; i++) {
+        operationButtons[i].addEventListener('click', handleOpButtons);
+}
+    
